d334b37f921d5b64a2c8ddc06eac8152762c62ea
ThElement12 <20170861@ce.pucmm.edu.do>
2021-10-20 08:45:01 -0400

    Se modifican algunos ajustes

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,5 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Joseph', apellido: 'De Los Santos', email: 'joe@doe.com', createAt: '05-10-2021'}
+  ];
