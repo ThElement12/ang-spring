999b3e776f0848623295bddb3eafdf4b15086e89
ThElement12 <20170861@ce.pucmm.edu.do>
2021-10-20 11:27:00 -0400

    Se inicializa el backend en spring

---
+++ spring-boot-back-apirest/src/main/java/com/angspring/springboot/backend/apirest/springbootbackapirest/SpringBootBackApirestApplication.java
@@ -0,0 +1,13 @@
+package com.angspring.springboot.backend.apirest.springbootbackapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackApirestApplication.class, args);
+    }
+
+}
---
+++ spring-boot-back-apirest/src/test/java/com/angspring/springboot/backend/apirest/springbootbackapirest/SpringBootBackApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angspring.springboot.backend.apirest.springbootbackapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
