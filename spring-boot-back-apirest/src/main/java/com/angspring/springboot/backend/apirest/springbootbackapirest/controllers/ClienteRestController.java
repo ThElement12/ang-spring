6ac9fa261610d1bfde74f4a279c7864c0dbbfb68
ThElement12 <20170861@ce.pucmm.edu.do>
2021-10-21 16:02:29 -0400

    Se modifica el controlador para listar uno y para crear un usuario

--- spring-boot-back-apirest/src/main/java/com/angspring/springboot/backend/apirest/springbootbackapirest/controllers/ClienteRestController.java
+++ spring-boot-back-apirest/src/main/java/com/angspring/springboot/backend/apirest/springbootbackapirest/controllers/ClienteRestController.java
@@ -3,10 +3,8 @@ package com.angspring.springboot.backend.apirest.springbootbackapirest.controlle
 import com.angspring.springboot.backend.apirest.springbootbackapirest.models.entity.Cliente;
 import com.angspring.springboot.backend.apirest.springbootbackapirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,15 @@ public class ClienteRestController {
     @GetMapping("/clientes")
     public List<Cliente> index(){
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public Cliente show(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.save(cliente);
     }
 }
