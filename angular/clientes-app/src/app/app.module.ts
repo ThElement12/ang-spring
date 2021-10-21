02312ac52bd74feb31365c9f1d7b73b17e732704
ThElement12 <20170861@ce.pucmm.edu.do>
2021-10-21 10:16:53 -0400

    Se implementa el cliente http

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
-
 import { ClienteService } from './clientes/cliente.service';
 
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
@@ -26,6 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -5,12 +5,19 @@ import { CLIENTES } from './clientes.json';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 @Injectable()
 export class ClienteService {
+  private urlEndpPoint:string = 'http://localhost:8080/api/clientes';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
   getClientes(): Observable<Cliente[]>{ 
-    return of(CLIENTES); 
+    //return of(CLIENTES); 
+    return this.http.get(this.urlEndpPoint).pipe(
+      map( (response) => response as Cliente[])
+    );
   }
 }
